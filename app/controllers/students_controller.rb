9db97a5951765e09f7484d86e4bf96d082b25f07
Ashley Wong <tofudragon333@gmail.com>
2022-08-13 09:56:10 -0400

    commit tree 1f96d656ef62513b9080a3a88c557b83b527a991

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,18 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    byebug
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
+    render json: students, status: :ok
+  end
+
+  def query
+    students = Student.find_by(params[:first_name])
+    byebug
     render json: students
   end
 
